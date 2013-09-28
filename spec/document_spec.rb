838aeb3fac24e09791ac32cf1801289da2ad7ff8
Ben Moss <ben@mossity.com>
2013-09-28 16:47:38 -0400

    Add test for doing multiple transforms of the same document

--- spec/document_spec.rb
+++ spec/document_spec.rb
@@ -5,24 +5,36 @@ include Transmute
 describe Document do
   describe "#transform" do
     it "applies a transformation to the subnodes that match the selector" do
-      document = Document.from_path("spec/fixtures/sample.html")
-      document.
-        transform("body") { |node| node.content = "sup" }.
-        to_html.should =~ /sup/
+      document = Document.
+        from_path("spec/fixtures/sample.html").
+        transform("body") { |node| node.content = "sup" }
+
+      expect(document.to_html).to match(/sup/)
     end
 
-    it "is immutable" do
+    it "returns a new document instead of modifying the original" do
       document = Document.from_path("spec/fixtures/sample.html")
       document.transform("body") { |node| node.content = "sup" }
-      document.to_html.should =~ /original content/
+      expect(document.to_html).to match(/original content/)
     end
 
     it "is chainable" do
-      document = Document.from_path("spec/fixtures/sample.html")
-      document.
-        transform("body") { |node| node.content = "sup" }.
-        transform("body") { |node| node.content = "dude" }.
-        to_html.should =~ /dude/
+      document = Document.
+        from_path("spec/fixtures/sample.html").
+        transform(".one") { |node| node.content = "sup" }.
+        transform(".two") { |node| node.content = "dude" }
+        expect(document.to_html).to match(/sup.*dude/m)
+    end
+  end
+
+  describe "#to_html" do
+    it "does not modify the underlying node" do
+      node = Node.from_path("spec/fixtures/sample.html")
+      document = Document.
+        new(node).
+        transform("body") { |node| node.content = "sup" }
+
+      document.to_html.should_not == node.to_html
     end
   end
 end
--- spec/transmute_spec.rb
+++ spec/transmute_spec.rb
@@ -1,10 +1,12 @@
 require_relative "../lib/transmute"
 
+include Transmute
+
 describe Transmute do
   describe ".html_resource" do
     it "returns a document from a file path" do
       document = Transmute.html_resource("spec/fixtures/sample.html")
-      document.to_html.should == File.read("spec/fixtures/sample.html")
+      document.should be_instance_of(Document)
     end
   end
 end
